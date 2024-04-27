5878aa0d5782060f5f07a935a680866a7353f183
Aleksei Polozhentsev <aleksei_polozhentsev@epam.com>
2024-04-27 19:43:44 +0400

    fix a bug in a minmax implementation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Noughts-and-Crosses
+
+Written in educational purposes.
+
+Game-Per-Month course, game #1, april '24.
+
+During this project I've learned GDScript basics (and learned that I'll probably stick to C#. I was missing abstracts classes/interfaces, multidimensional arrays and fuller static typing support).
+Probably, more valuable experience was to try writing AI first time, and, specifically, learning minimax algorithm. Debugging it was painful.
--- scripts/ai/MinMaxStrategy.gd
+++ scripts/ai/MinMaxStrategy.gd
@@ -5,7 +5,7 @@ var checker: WinChecker = WinChecker.new()
 
 # best move for O is negative
 func selectCell(board: Array2D) -> Dictionary:
-	var bestScore = null
+	var bestScore
 	var moveScore
 	var bestMove: Dictionary = {"x": null, "y": null}
 	var boardCopy = board.getCopy();
@@ -33,25 +33,22 @@ func getMoveScore(board: Array2D, moveX: int, moveY: int, playerMark: int):
 		board.setValue(moveX, moveY, 0)
 		return 0
 	
-	var score = null
+	var score
 	var moveScore
+	var nextMovingPlayer = -playerMark
 
 	for x in 3:
 		for y in 3:
 			if board.getValue(x, y) == 0:
-				moveScore = getMoveScore(board, x, y, -playerMark)
-				
-				if moveScore == playerMark:
+				moveScore = getMoveScore(board, x, y, nextMovingPlayer)
+				if moveScore == nextMovingPlayer:
 					board.setValue(moveX, moveY, 0)
 					return moveScore
-					
-				if score == null:
+				elif score == null:
 					score = moveScore
-					continue
-						
-				if playerMark == 1 && moveScore > score:
+				elif nextMovingPlayer == 1 && moveScore > score:
 					score = moveScore
-				elif playerMark == -1 && moveScore < score:
+				elif nextMovingPlayer == -1 && moveScore < score:
 					score = moveScore
 		
 	board.setValue(moveX, moveY, 0)
